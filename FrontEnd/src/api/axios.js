3b0cd0191d8a809f791782dc2618919a2550dac6
SSE <ssedevca@gmail.com>
2024-12-10 21:16:22 -0500

    Deployed front end and backend

--- FrontEnd/src/api/axios.js
+++ FrontEnd/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Backend URL
+  baseURL: 'https://backend-service-1091173744770.us-east1.run.app', // Backend URL
 });
 
 export default api;
